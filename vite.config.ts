77caee9abb3f0eb3424ab5d7fe5d79e3d045b0a2
michael-255 <kodiakchaos@gmail.com>
2025-06-22 15:05:02 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vueDevTools(),
+    quasar({
+      autoImportComponentCase: 'kebab',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/lifestyle/',
+})
